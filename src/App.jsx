740e40f3a8497909fc9d782aaf64a2bd7f4ccfd4
DANKAMN <danielnicholas1477@gmail.com>
2025-05-01 10:58:32 +0100

    Add functionality for searching movies from the movie api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,61 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Search from './components/Search'
+import Loader from './components/Loader';
 
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+
+const API_KEY = import.meta.env.VITE_IMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok) {
+        throw new Error('Failed to fetch movies');
+      }
+
+      const data = await response.json();
+
+      if(data.response === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([])
+        return;
+      }
+
+      setMovieList(data.results || [])
+      
+    } catch(error) {
+      console.log(`Error fetching moviess: ${error}`);
+      setErrorMessage('Error fetching movies. Try another time')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  }, [])
+  
 
   return (
     <main className="">
@@ -13,10 +65,27 @@ function App() {
         <header>
           <img src="./hero.png" alt="hero-banner" />
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Stress</h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <h1 className='text-white'>{searchTerm}</h1>
+        <section className='all-movies'>
+          <h2 className='mt-[30px]'>All Movies</h2>
+
+          {isLoading ? (
+            <p className="text-white">
+              <Loader />
+            </p>
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <p className='text-white' key={movie.id}>{movie.title}</p>
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
     </main>
   )
---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Spinner } from "flowbite-react";
+
+const Loader = () => {
+  return <Spinner color="purple" aria-label="Purple spinner example" />
+}
+
+export default Loader
